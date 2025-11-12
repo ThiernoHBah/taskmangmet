034108ed9dcd3c2456483880fde3ae981fcae4ec
thiernohbah <thiernobah2001@gmail.com>
2025-11-12 00:07:46 -0500

    ive identifed my elements and gave a funcion for my button if they dont input noting an alert will pop up

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+let tasks = [];
 let taskName = document.getElementById("taskName");
 let taskCategory = document.getElementById("taskCategory");
 let taskDeadline = document.getElementById("taskDeadline");
 let addTaskBtn = document.getElementById("addTaskBtn");
 let taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", function() {
+  let name = nameInput.value;
+  let category = catInput.value;
+  let date = dateInput.value;
+
+  if (name === "" || category === "" || date === "") {
+    alert("Please input tasks here");
+    return;
+  }
