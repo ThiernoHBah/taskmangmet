06dd67d914fba98c07e83d8b0a5765357f1d0957
thiernohbah <thiernobah2001@gmail.com>
2025-11-12 00:01:38 -0500

    i identified my funcions

---
+++ script.js
@@ -0,0 +1,5 @@
+let taskName = document.getElementById("taskName");
+let taskCategory = document.getElementById("taskCategory");
+let taskDeadline = document.getElementById("taskDeadline");
+let addTaskBtn = document.getElementById("addTaskBtn");
+let taskList = document.getElementById("taskList");
