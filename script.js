debf128d46946a03ecedcc18d554f626baf3be6d
thiernohbah <thiernobah2001@gmail.com>
2025-11-12 00:15:28 -0500

    gavw it a loop if task is complet

--- script.js
+++ script.js
@@ -5,12 +5,33 @@ let taskDeadline = document.getElementById("taskDeadline");
 let addTaskBtn = document.getElementById("addTaskBtn");
 let taskList = document.getElementById("taskList");
 
-addBtn.addEventListener("click", function() {
+addBtn.addEventListener("click"), function(){
   let name = nameInput.value;
   let category = catInput.value;
   let date = dateInput.value;
 
   if (name === "" || category === "" || date === "") {
-    alert("Please input tasks here");
+    alert("Please input task");
     return;
-  }
+}
+
+    for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].status !== "Completed" && tasks[i].date < today) {
+      tasks[i].status = "Overdue";
+    }
+
+    let li = document.createElement("li");
+    li.textContent = tasks[i].name + " (" + tasks[i].category + ") - " + tasks[i].date + " - " + tasks[i].status;
+
+    // make a done button
+    let doneBtn = document.createElement("button");
+    doneBtn.textContent = "Done";
+    doneBtn.onclick = function() {
+      tasks[i].status = "Completed";
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      showTasks();
+    };
+
+    li.appendChild(doneBtn);
+    taskList.appendChild(li);
+  
