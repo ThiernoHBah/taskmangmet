d50bf6574955bb89f48146518c601b6008183be6
thiernohbah <thiernobah2001@gmail.com>
2025-11-12 00:21:16 -0500

    complete product

--- script.js
+++ script.js
@@ -5,25 +5,53 @@ let taskDeadline = document.getElementById("taskDeadline");
 let addTaskBtn = document.getElementById("addTaskBtn");
 let taskList = document.getElementById("taskList");
 
-addBtn.addEventListener("click"), function(){
-  let name = nameInput.value;
-  let category = catInput.value;
-  let date = dateInput.value;
+// load saved tasks
+if (localStorage.getItem("tasks")) {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+  showTasks();
+}
+
+// when add button clicked
+addTaskBtn.addEventListener("click", function() {
+  let name = taskName.value;
+  let category = taskCategory.value;
+  let date = taskDeadline.value;
 
   if (name === "" || category === "" || date === "") {
     alert("Please input task");
     return;
-}
+  }
 
-    for (let i = 0; i < tasks.length; i++) {
+  let task = {
+    name: name,
+    category: category,
+    date: date,
+    status: "In Progress"
+  };
+
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  showTasks();
+
+  // clear boxes
+  taskName.value = "";
+  taskCategory.value = "";
+  taskDeadline.value = "";
+});
+
+function showTasks() {
+  taskList.innerHTML = "";
+
+  let today = new Date().toISOString().split("T")[0];
+
+  for (let i = 0; i < tasks.length; i++) {
     if (tasks[i].status !== "Completed" && tasks[i].date < today) {
       tasks[i].status = "Overdue";
     }
 
     let li = document.createElement("li");
-    li.textContent = tasks[i].name + " (" + tasks[i].category + ") - " + tasks[i].date + " - " + tasks[i].status;
+    li.textContent = tasks[i].name + " (" + tasks[i].category + ") - " + tasks[i].date + " - " + tasks[i].status + " ";
 
-    // make a done button
     let doneBtn = document.createElement("button");
     doneBtn.textContent = "Done";
     doneBtn.onclick = function() {
@@ -34,4 +62,7 @@ addBtn.addEventListener("click"), function(){
 
     li.appendChild(doneBtn);
     taskList.appendChild(li);
-  
+  }
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
